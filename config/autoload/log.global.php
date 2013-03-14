4037139a67466ea7e2edd7150e3d952cb2ecc76f
Emanuel Rylke <er@cloudcontrol.de>
2013-03-14 13:51:26 +0100

    Put $deployment into right place

--- config/autoload/log.global.php
+++ config/autoload/log.global.php
@@ -1,13 +1,12 @@
 <?php
 
-$deployment = explode('/', $_ENV['DEP_NAME'])[1];
-
 return array(
     'service_manager' => array(
         'factories' => array(
             'Zend\Log\Logger' => function(){
                 $logger = new Zend\Log\Logger;
                 $writer = new Zend\Log\Writer\Syslog();
+		$deployment = explode('/', $_ENV['DEP_NAME'])[1];
 		if($deployment === 'default') {
 		    $writer->addFilter(Zend\Log\Logger::ERR);
 		}
