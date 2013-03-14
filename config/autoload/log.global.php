c83505a93726f4b789254ddb52afc135b792a057
Emanuel Rylke <er@cloudcontrol.de>
2013-03-14 13:18:12 +0100

    Restrict logging to errors only on default deployment.

--- config/autoload/log.global.php
+++ config/autoload/log.global.php
@@ -1,12 +1,16 @@
 <?php
 
+$deployment = explode('/' $_ENV['DEP_NAME'])[1]
+
 return array(
     'service_manager' => array(
         'factories' => array(
             'Zend\Log\Logger' => function(){
                 $logger = new Zend\Log\Logger;
                 $writer = new Zend\Log\Writer\Syslog();
-                $writer->addFilter(Zend\Log\Logger::ERR);
+		if($deployment === 'default') {
+		    $writer->addFilter(Zend\Log\Logger::ERR);
+		}
                 $logger->addWriter($writer);
                 return $logger;
             },
